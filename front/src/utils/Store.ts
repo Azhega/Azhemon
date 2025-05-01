5ba43322477cc1a87ef536f0697aa572407ca4b4
Azhega <lethan986@gmail.com>
2025-05-01 16:37:22 +0200

    feat : Add EventBus and Store (state management)
    EventBus to manage events in the game
    Store to store the state of the game

---
+++ front/src/utils/Store.ts
@@ -0,0 +1,66 @@
+import EventBus from './EventBus';
+
+type Listener = (state: any) => void;
+
+export class Store {
+  private static instance: Store;
+  private state: any = {};
+  private listeners: Listener[] = [];
+  
+  private constructor() {
+    // État initial
+    this.state = {
+      game: {
+        screen: 'menu', // 'menu', 'teambuilder', 'battle'
+        isLoading: false
+      },
+      battle: null,
+      teams: [],
+      pokemonSpecies: []
+    };
+  }
+  
+  static getInstance(): Store {
+    if (!Store.instance) {
+      Store.instance = new Store();
+    }
+    return Store.instance;
+  }
+  
+  getState(): any {
+    // Copy ensures immutability, predictability and encapsulation
+    return { ...this.state }; 
+  }
+  
+  setState(newState: any): void {
+    const oldState = { ...this.state };
+    this.state = { ...this.state, ...newState };
+    
+    // Emits events for each top level key that has changed
+    Object.keys(newState).forEach(key => {
+      if (oldState[key] !== this.state[key]) {
+        EventBus.emit(`store:${key}:updated`, this.state[key]);
+      }
+    });
+    
+    this.notify();
+  }
+  
+  subscribe(listener: Listener): () => void {
+    this.listeners.push(listener);
+    
+    // Return an unsubscribe function
+    return () => {
+      const index = this.listeners.indexOf(listener);
+      if (index > -1) {
+        this.listeners.splice(index, 1);
+      }
+    };
+  }
+  
+  private notify(): void {
+    this.listeners.forEach(listener => listener(this.getState()));
+  }
+}
+
+export default Store.getInstance();
