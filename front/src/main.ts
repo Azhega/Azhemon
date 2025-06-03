6ccbfe3a3f755d35db23edce1dff4ba3d524933e
Azhega <lethan986@gmail.com>
2025-06-03 20:47:04 +0200

    style : menu.css
    Added menu.css to style the new Team Selector

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,7 @@
 import './public/styles/main.css';
 import './public/styles/teambuilder.css';
 import './public/styles/battle.css';
+import './public/styles/menu.css';
 import { GameController } from './controllers/GameController.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
