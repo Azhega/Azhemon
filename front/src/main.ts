e4024c0ae1f886f9ef0ab8c2bbf22ce4b53ac1bd
Azhega <lethan986@gmail.com>
2025-06-09 12:24:16 +0200

    style : Login Page CSS
    First look of Login Page CSS, might change later

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,4 +1,5 @@
 import './public/styles/main.css';
+import './public/styles/login.css';
 import './public/styles/teambuilder.css';
 import './public/styles/battle.css';
 import './public/styles/menu.css';
